a9b82eb9e4d07e978c920ab100270d47a1139017
sebbycorp <sebastian@maniak.io>
2023-10-13 10:41:38 -0400

    security group

--- terraform/juiceshop.tf
+++ terraform/juiceshop.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "juiceshop" {
   count                  = 1
   instance_type          = "t2.micro"
   ami                    = "ami-087c17d1fe0178315"
-  vpc_security_group_ids = aws_security_group.juiceshop_sg.id
-  subnet_id              =  aws_subnet.mgmt.id
+  vpc_security_group_ids = [aws_security_group.juiceshop_sg.id]
+  subnet_id              = aws_subnet.mgmt.id
   key_name               = "demokeyforaws"
   user_data              = file("juice.sh")
 
@@ -22,10 +22,20 @@ resource "aws_security_group" "juiceshop_sg" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    description = "HTTPS"
+    description = "HTTP"
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  
+
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 }
+
