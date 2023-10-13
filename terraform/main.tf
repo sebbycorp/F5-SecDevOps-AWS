88bc5896ae018d7563e2f8693c141db89064de5c
sebbycorp <sebastian@maniak.io>
2023-10-13 11:38:03 -0400

    updates

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">=0.14.9"
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    encrypt        = true
+    bucket         = "f5-secdevops-aws-terraform-state"
+    key            = "secdevops-tfstate-s3-bucket"
+    region         = "us-east-1"
+    dynamodb_table = "f5-secdevops-aws-state-lock-dynamo"
+  }
+}
+
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "f5-secdevops-aws-terraform-state"
+}
+
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "f5-secdevops-aws-terraform-state-lock-dynamo"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+  tags = {
+    Name = "DynamoDB f5-secdevops-aws State"
+  }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
