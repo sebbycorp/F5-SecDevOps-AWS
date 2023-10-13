472d5c28a7f856adebf159f415862ce23b4f739c
sebbycorp <sebastian@maniak.io>
2023-10-13 15:58:08 -0400

    ttest

--- terraform/juiceshop.tf
+++ terraform/juiceshop.tf
@@ -43,7 +43,7 @@ module "AppDeploy" {
   tenant               = "juiceshop"
   as3tmpl              = "http"
   common_name          = "juiceshop"
-  vip_address          = module.bigip.private_ip[0]
+  vip_address          = "10.0.1.158"
   pool_members_port    = "3000"
   monitor              = "tcp"
   load_balancing_mode  = "least-connections-member"
