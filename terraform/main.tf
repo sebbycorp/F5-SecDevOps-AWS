a4b3d966a4d94659d4c5c0773ea6f915d18da45d
sebbycorp <sebastian@maniak.io>
2023-10-13 11:42:16 -0400

    version

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "5.21.0"
     }
   }
 
-  required_version = ">=0.14.9"
+  required_version = ">=1.5.0"
 
 }
 
